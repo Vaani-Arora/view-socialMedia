cd60eef228bee2001ab401aeb9849574ab84cbb4
Vaani-Arora <115731505+Vaani-Arora@users.noreply.github.com>
2023-03-17 19:10:36 +0530

    Update UserImage.jsx

--- app/src/components/UserImage.jsx
+++ app/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
                 width={size}
                 height={size}
                 alt="user"
-                src={`https://view-backend.onrender.com/assets/${image}`}
+                src={`https://localhost:3000/assets/${image}`}
             />
         </Box>
     );
